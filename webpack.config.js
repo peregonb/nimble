8f34365bc93790f5a9e4790e354797664ebafab9
Peregon Bogdan <peregonbw@gmail.com>
2020-02-18 18:47:06 +0200

    adding sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: ['babel-loader']
-    }]
-  },
-  devServer: {
-    contentBase:  path.resolve(__dirname, 'dist'),
-    port: 9000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html" //source html
-    })
-  ]
+    entry: path.resolve(__dirname, 'src/index'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: ['babel-loader']
+        },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader"}
+                ]
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 1488
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html" //source html
+        })
+    ]
 };
